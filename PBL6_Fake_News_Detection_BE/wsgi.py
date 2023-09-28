b23a3b096e7e333e321f57669fb0390cc6874df7
ductran <93823088+tranhongduc@users.noreply.github.com>
2023-09-28 22:41:00 +0700

    first

---
+++ PBL6_Fake_News_Detection_BE/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PBL6_Fake_News_Detection_BE project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PBL6_Fake_News_Detection_BE.settings')
+
+application = get_wsgi_application()
